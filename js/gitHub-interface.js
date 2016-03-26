496764c4f260515ef308ebb4cc336541a981937d
AndrewTelleria <atelleria08@gmail.com>
2016-03-26 13:05:19 -0700

    json response with requested information

--- js/gitHub-interface.js
+++ js/gitHub-interface.js
@@ -1,12 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
-
-
-
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+$(document).ready(function(){
+	$('#userInput').click(function(){
+		var userName = $('#userName').val();
+		
+		  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+		  	
+		    console.log(response);
+		  }).fail(function(error){
+		    console.log(error.responseJSON.message);
+		  });
+	});
+});
