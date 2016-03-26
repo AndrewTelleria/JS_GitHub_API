fb3ae3ef3278872cbafc430e0b09e15219e56e40
AndrewTelleria <atelleria08@gmail.com>
2016-03-26 13:11:27 -0700

    Input username and provide name, email, # of followers.

--- js/gitHub-interface.js
+++ js/gitHub-interface.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
 		var userName = $('#userName').val();
 		
 		  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-		  	
+		  	$('.fullName').text('Name:' + response.name + '.');
+		  	$('.followers').text('# of Followers:' + response.followers);
+		  	$('.email').text('Email:' + response.email );
 		    console.log(response);
 		  }).fail(function(error){
 		    console.log(error.responseJSON.message);
