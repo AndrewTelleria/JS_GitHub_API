2d8006d635221cd19578f697bab1b5d14421878d
AndrewTelleria <atelleria08@gmail.com>
2016-03-28 20:06:00 -0700

    touch up on interface js file

--- js/gitHub.js
+++ js/gitHub.js
@@ -2,7 +2,7 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getInformation = function(userName) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    $('.avatar').empty();
+    
     $('.avatar').append('<img src =' + response.avatar_url + ' alt= "Avatar picture" />');
     $('.fullName').text('Name: ' + response.name + '.');
     $('.followers').text('# of Followers: ' + response.followers);
@@ -13,7 +13,7 @@ exports.getInformation = function(userName) {
 
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     for(i = 0; i < response.length; i++){
-      // $('.repos').empty();
+      
       $('.repos').append('<li><b>Repos:</b> ' + response[i].name + '</li>')
       $('.repos').append('<li><b>Description:</b> ' + response[i].description + '</li>')
     }
